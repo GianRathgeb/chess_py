ad68802a3f857e690e06bb3e9d19712c520cfc84
GianRathgeb <gian.rathgeb@bbz-sh.ch>
2021-03-01 12:55:29 +0100

    defined board without figures

--- models/figure.py
+++ models/figure.py
@@ -6,7 +6,14 @@ class Figure():
 
 
 class Board():
-    def __init__(self, boardWith = 8, boardHight = 8):
-        self.boardWith = boardWith
+    def __init__(self, boardWidth = 8, boardHight = 8):
+        self.boardWith = boardWidth
         self.boardHight = boardHight
         self.board = []
+        for width in range(0, boardHight):
+            self.board.append([])
+            for height in range(0, boardHight):
+                self.board[width].append(None)
+
+print(Board().board)
+
